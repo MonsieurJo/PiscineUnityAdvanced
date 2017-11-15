cff37bda29e06faaec90e819c5731009a29ee150
Jordan <j.drespincho@yahoo.com>
2017-11-15 11:27:38 +0100

    qd

---
+++ Assets/Script/DestructibleObstacle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructibleObstacle : MonoBehaviour {
+
+    public float maxHealth = 100f;
+
+    private float _currentHealth;
+
+	// Use this for initialization
+	void Start () {
+        _currentHealth = maxHealth;
+	}
+	
+	public void TakeDamage(float damage, GameObject instigator)
+    {
+        if (_currentHealth <= 0f)
+        {
+            Kill();
+        }
+    }
+
+    public void Kill()
+    {
+        Destroy(this.gameObject);
+    }
+}
